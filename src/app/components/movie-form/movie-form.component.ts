84beb0d237117742d42aa0a422c5004071914aac
Abdulmalek Al-Gahmi <aalgahmi@gmail.com>
2020-02-03 20:00:06 -0700

    After class 1/30

--- src/app/components/movie-form/movie-form.component.ts
+++ src/app/components/movie-form/movie-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from 'src/app/services/movie.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-form',
@@ -20,7 +20,38 @@ export class MovieFormComponent implements OnInit {
       id: [null],
       title: [null, Validators.required],
       year: [2020, [Validators.required, Validators.min(1900), Validators.max(2020)]],
+      rated: [null, Validators.required],
+      genre: [null, Validators.required],
+      plot: [null, Validators.required],
+      poster: [null, [Validators.required, Validators.pattern(/https?:\/\/.+(svg|jpg|png|gif)/)]],
+      rating: [null, Validators.pattern(/^1?\d(\.\d\d)?$/)],
+      votes: [0, Validators.pattern(/^\d+$/)],
+      imdbID: [null, Validators.required]
     })
+
+    this.isEdit = !!this.route.snapshot.params['id']
+    if(this.isEdit){
+      this.movieService.getMovie(this.route.snapshot.params['id']).subscribe(
+        m => this.movieForm.patchValue(m)
+      )
+    }
+  }
+
+
+  get title() { return this.movieForm.get('title') }
+  get year() { return this.movieForm.get('year') }
+  get rated() { return this.movieForm.get('rated') }
+  get genre() { return this.movieForm.get('genre') }
+  get plot() { return this.movieForm.get('plot') }
+  get poster() { return this.movieForm.get('poster') }
+  get rating() { return this.movieForm.get('rating') }
+  get votes() { return this.movieForm.get('votes') }
+  get imdbID() { return this.movieForm.get('imdbID') }
+
+  submitForm(f : NgForm){
+    if(f.valid){
+
+    }
   }
 
 }
